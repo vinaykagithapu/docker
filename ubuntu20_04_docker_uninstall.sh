534bc34a5ca6c9a958df3f265e7b4a5b4b5458fd
vinaykagithapu <33367418+vinaykagithapu@users.noreply.github.com>
2021-12-10 19:48:34 +0530

    Update ubuntu20_04_docker_uninstall.sh

--- ubuntu20_04_docker_uninstall.sh
+++ ubuntu20_04_docker_uninstall.sh
@@ -1,7 +1,7 @@
 echo "\nRemoving Docker and their Dependencies\n"
-sudo apt remove docker.io bridge-utils containerd pigz runc ubuntu-fan liberror-perl -y
+sudo apt remove docker.io bridge-utils containerd pigz runc ubuntu-fan -y
 echo "\nRemoving configuration\n"
-sudo apt purge docker.io bridge-utils containerd pigz runc ubuntu-fan liberror-perl -y
+sudo apt purge docker.io bridge-utils containerd pigz runc ubuntu-fan -y
 echo "\n\Cleaning up files\n"
 sudo rm -rf /etc/docker
 echo "\nDocker Uninstall Finished\n"
